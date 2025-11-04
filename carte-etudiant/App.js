491aa2ae92afc81394e95f3cca4c89e52d2b30de
SoufianeMoussaoui <dgiugfha098@gmail.com>
2025-11-04 20:25:55 +0100

    add carte-etudiant project

---
+++ carte-etudiant/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StatusBar, StyleSheet } from 'react-native';
+import { CardList } from './components/CardList';
+
+import { CardList } from './CardList';
+
+
+
+export default function App() {
+  return (
+    < View style={styles.screen}>
+      <StatusBar />
+      <CardList />
+    </ View >
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: { flex: 1, backgroundColor: '#f6f7fb' }
+});
+
